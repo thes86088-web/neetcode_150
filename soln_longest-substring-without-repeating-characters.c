466580714902740ddd33afef30f5570edbbdb554
durvesh <thes86088@gmail.com>
2026-07-13 09:57:53 +0530

    add print statements to track values

--- soln_longest-substring-without-repeating-characters.c
+++ soln_longest-substring-without-repeating-characters.c
@@ -9,7 +9,8 @@ question link : http://leetcode.com/problems/longest-substring-without-repeating
 int lengthOfLongestSubstring(char* s) {
     int visited[256] = {0};
     int stringLength = ( sizeof(s) )/( sizeof(*s) );
-    printf( " %d \n", stringLength );
+    //add print statements to check values stored in variables
+    printf( "stringLength : %d \n", stringLength );
     int start = 0, end = start;
     for( int i=0; i<stringLength; i++ ){
         if( visited[ s[i] ] == 0 ){
@@ -20,6 +21,9 @@ int lengthOfLongestSubstring(char* s) {
             start = i;
             end = i;
         }
+        //add print statements to track values of start and end
+        //problem : if a visited cahharacter is found, start resets again without storing previous values
+        printf( " i = %d, start = %d, end = %d \n", i, start, end );
     }
     char* result = calloc( end-start, sizeof(char) );
     result = s+start;
@@ -30,5 +34,5 @@ int lengthOfLongestSubstring(char* s) {
 int main(){
     char *s = "abcabcbb";
     int len = lengthOfLongestSubstring(s);
-    printf( " %d ", len );
+    printf( "len : %d ", len );
 }
